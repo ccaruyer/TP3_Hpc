fce5f2f360b8eeead07cc73e3d99639483f88e6a
Caruyer Claire <claire.caruyer@users.noreply.github.com>
2022-06-20 20:15:26 +0200

    update exercice 1.1

--- Src/Camera.cpp
+++ Src/Camera.cpp
@@ -1,5 +1,6 @@
 #include "Camera.hpp"
 #include "Rayon.hpp"
+#include <thread>
 
 
 Camera::Camera(){
@@ -65,42 +66,39 @@ ostream& operator<<(ostream& out, const Camera& c){
 
 void Camera::genererImageParallele(const Scene& sc, Image& im, int profondeur, int nbThreads){
 
-    // Calcul des dimensions d'un pixel par rapport
-    // à la résolution de l'image - Les pixels doivent être carrés
-    // pour éviter les déformations de l'image.
-    // On fixe :
-    // - les dimensions en largeur de l'écran seront comprises dans [-1, 1]
-    // - les dimensions en hauteur de l'écran soront comprises dans [-H/L, H/L]
-
+    //hauteur de l'image = im.getHauteur()/3
+    //largeur de l'image = im.getLargeur()
     float cotePixel = 2.0/im.getLargeur();
 
+    double H = im.getHauteur()/nbThreads;
 
+    for (int t = 1; t < nbThreads; t++) {
 
-    // Pour chaque pixel
-    for(int i=0; i<im.getLargeur(); i++){
-        for(int j=0; j<im.getHauteur(); j++){
+        // Pour chaque pixel
+        for (int i = 0; i < im.getLargeur(); i++) {
+            for (int j = 0 ; j < (H * t); j++) {
 
-            // calcul des coordonnées du centre du pixel
-            float milieuX = -1 + (i+0.5f)*cotePixel;
-            float milieuY =  (float)im.getHauteur()/(float)im.getLargeur()
-                             - (j+0.5f)*cotePixel;
+                // calcul des coordonnées du centre du pixel
+                float milieuX = -1 + (i + 0.5f) * cotePixel;
+                float milieuY = (float) (im.getHauteur()/nbThreads) / (float) im.getLargeur()
+                                - (j + 0.5f) * cotePixel;
 
-            Point centre(milieuX, milieuY, 0);
+                Point centre(milieuX, milieuY, 0);
 
-            // Création du rayon
-            Vecteur dir(position, centre);
-            dir.normaliser();
-            Rayon ray(position, dir);
+                // Création du rayon
+                Vecteur dir(position, centre);
+                dir.normaliser();
+                Rayon ray(position, dir);
 
-            // Lancer du rayon primaire
-            Intersection inter;
-            if(sc.intersecte(ray, inter)){
-                im.setPixel(i, j, inter.getCouleur(sc, position, profondeur));
-            }
-            else
-                im.setPixel(i, j, sc.getFond());
+                // Lancer du rayon primaire
+                Intersection inter;
+                if (sc.intersecte(ray, inter)) {
+                    im.setPixel(i, j, inter.getCouleur(sc, position, profondeur));
+                } else
+                    im.setPixel(i, j, sc.getFond());
 
-        }// for j
+            }// for j
 
-    }// for i
+        }// for i
+    }
 }
